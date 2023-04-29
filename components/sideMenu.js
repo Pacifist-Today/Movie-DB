18defdf2565420a8c04e7417cc5517d321fb5a1b
Pacifist <desmondmorales92@gmail.com>
2023-04-29 17:08:09 +0300

    main components

---
+++ components/sideMenu.js
@@ -0,0 +1,16 @@
+import React, {useState} from "react";
+
+const SideMenu = (props) => {
+    return (
+        <>
+            <h1 className="my-4">{props.appName}</h1>
+            <div className="list-group">
+                <a href="#" className="list-group-item">Category 1</a>
+                <a href="#" className="list-group-item">Category 2</a>
+                <a href="#" className="list-group-item">Category 3</a>
+            </div>
+        </>
+    )
+}
+
+export default SideMenu
