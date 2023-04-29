ceed438031605ab6c8d89bd3267b93cb8ff38329
Pacifist <desmondmorales92@gmail.com>
2023-04-29 18:29:48 +0300

    actions index.js

---
+++ pages/about.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+// const About = () => {
+//     const message = 'Hello World'
+//     return (
+//         <h1>Hello About page – {message}</h1>
+//     )
+// }
+
+class About extends React.Component{
+    render() {
+        return (
+            <h1>Hello I am class component</h1>
+        )
+    }
+}
+
+
+export default About
