b2ca114a9ee71440a3ccca3ab6b0811226d0df40
Pacifist <desmondmorales92@gmail.com>
2023-05-08 19:15:11 +0300

    posts

--- actions/index.js
+++ actions/index.js
@@ -50,3 +50,9 @@ export const deleteMovie = (id) => {
             .delete(`${BASE_URL}/api/v1/movies/${id}`)
             .then(res => res.data)
 }
+
+export const getPosts = (id) => {
+    return axios
+        .get(`${BASE_URL}/api/v1/posts`)
+        .then(res => res.data)
+}
