da8f97f425803a75c31f15666c17c257f14d0e47
Pacifist <desmondmorales92@gmail.com>
2023-05-12 23:05:20 +0300

    burger handler in nav menu

--- components/navbar.js
+++ components/navbar.js
@@ -4,43 +4,64 @@ import {useEffect, useState} from "react";
 const Navbar = () => {
 
     const [isNavBurgerActive, setNavBurgerState] = useState(false)
-    const [burgerStyle, setBurgerStyle] = useState("display: 'none'")
-
-    console.log(isNavBurgerActive)
+    const [isDropDownActive, setDropDownState] = useState(false)
 
     const burgerHandler = () => {
         setNavBurgerState(!isNavBurgerActive)
-        setBurgerStyle(burgerStyle === "display: 'none'" ? "display: 'static'" : "display: 'none'")
+    }
+
+    const dropDownHandler = () => {
+        setDropDownState(!isDropDownActive)
     }
 
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
             <div className="container-fluid">
                 <a className="navbar-brand" href="#">Movie DB</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
-                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                        aria-expanded="false" aria-label="Toggle navigation">
+                <button onClick={burgerHandler}
+                        className={`navbar-toggler ${!isNavBurgerActive ? 'collapsed' : ''}`}
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent"
+                        aria-controls="navbarSupportedContent"
+                        aria-expanded={isNavBurgerActive}
+                        aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon" />
                 </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <div className={`collapse navbar-collapse ${isNavBurgerActive ? 'show' : ''}`}>
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
                              <Link legacyBehavior href="/">
-                                 <a className="nav-link" href="#">Home
-                                     <span className="sr-only">(current)</span>
-                                 </a>
+                                 <a className="nav-link" href="#">Home</a>
                              </Link>
-                            {/*<a className="nav-link active" aria-current="page" href="#">Home</a>*/}
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">About</a>
+                            <Link legacyBehavior href="/about">
+                                <a className="nav-link" href="#">About</a>
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link legacyBehavior href="/contact">
+                                <a className="nav-link" href="#">Contact</a>
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link legacyBehavior href="/posts">
+                                <a className="nav-link" href="#">Posts</a>
+                            </Link>
                         </li>
                         <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                               aria-expanded="false">
+                            <a
+                                className={`nav-link dropdown-toggle ${isDropDownActive ? 'show' : ''}`}
+                                href="#"
+                                role="button"
+                                onClick={dropDownHandler}
+                                data-bs-toggle="dropdown"
+                                aria-expanded={isDropDownActive}
+                            >
                                 Dropdown
                             </a>
-                            <ul className="dropdown-menu">
+                            <ul className={`dropdown-menu ${isDropDownActive ? 'show' : ''}`}>
                                 <li><a className="dropdown-item" href="#">Movies</a></li>
                                 <li><a className="dropdown-item" href="#">Series</a></li>
                                 <li>
